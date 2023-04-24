8313363dc4d1399735dfd4bc8797d9a9205b1042
oyinlola <oyinlola@eksusmartschool.com.ng>
2023-04-25 00:43:00 +0100

    Final commit on employee management system

--- src/main/java/com/alash/springboot/exception/ResourceNotFoundException.java
+++ src/main/java/com/alash/springboot/exception/ResourceNotFoundException.java
@@ -1,2 +1,14 @@
-package com.alash.springboot.exception;public class ResourceNotFoundException {
+package com.alash.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
